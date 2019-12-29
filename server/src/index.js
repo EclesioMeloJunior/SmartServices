e68dcf1bf3261d5fa1abe404a6f293521eb25ae5
eclesiomelojunior <eclesiomelo.1@gmail.com>
2019-12-29 01:10:37 -0400

    app em server separated and env file done

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,21 @@
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('../config');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (_, response) => {
+  response.json({
+    message: `Welcome to SmartService`,
+    version: config.appVersion,
+    environment: config.environment
+  });
+});
 
 module.exports = app;
